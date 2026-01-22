0c385fa80790bdba3295f3574a014a53f95a3580
Emika Sandina <emikasandina06@gmail.com>
2026-01-22 11:31:56 +0530

    designed the citizen home page structure + navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import CitizenHome from "./components/citizen/Home Page/CitizenHome";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/citizen/home" />} />
+        <Route path="/citizen/home" element={<CitizenHome />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
