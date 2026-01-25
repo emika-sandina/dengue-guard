352d4e5224d1738685d579bbbd3923b820da0f0f
Senuthmi <tsenuthmi2007@gmail.com>
2026-01-25 20:56:09 +0530

    Add env example for chatbot configuration

--- backend/src/chatbot.js
+++ backend/src/chatbot.js
@@ -1,8 +1,12 @@
+// Import required packages
 let express=require('express');
 let app=express();
 let cors=require('cors')
+
 app.use(cors());
 app.use(express.json())
+
+//Import google gemini AI and set the model
 let {GoogleGenerativeAI}=require('@google/generative-ai');
 const path=require('path');
 require('dotenv').config({path: path.resolve(__dirname, '.env') })
@@ -10,12 +14,15 @@ require('dotenv').config({path: path.resolve(__dirname, '.env') })
 let genAI=new GoogleGenerativeAI(process.env.GEMINIKEY)
 let model=genAI.getGenerativeModel({model:'gemini-2.5-flash'});
 
+//API endpoint to handle requests
 app.post('/ask',
     async(req,res)=>{
         let {input}=req.body;
         let data=await model.generateContent(input)
+        //fetch the response
         let finalData=data.response.text()
 
+        //Send response back to frontend
         res.send({
             _status:true,
             _message:"Content Found",
@@ -25,17 +32,11 @@ app.post('/ask',
     }
 )
 
-// Frontend Routing
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
-
 const PORT = process.env.PORT;
+//Start the server
 app.listen(PORT,()=>{
     console.log(`Server Start on port ${PORT}`);
 })
 
-// ✅ Export the app to be used by server.js
+// Export the app to be used by server.js
 module.exports = app;
