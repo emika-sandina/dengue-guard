92768c075b2983ad2e3bcc7c095a90689d75f540
Emika Sandina <emikasandina06@gmail.com>
2026-01-31 00:24:53 +0530

    did the ui for report breeding sites page, placed the home pages of citizen and moh inside the pages folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,9 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import CitizenHome from "./components/citizen/Home Page/CitizenHome";
-
+import CitizenHome from "./pages/citizen/Home Page/CitizenHome";
+import MOHHome from "./pages/moh/Home Page/MOHHome";
+import ReportSites from "./pages/citizen/Report Breeding Sites/ReportSites";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/citizen/home" />} />
         <Route path="/citizen/home" element={<CitizenHome />} />
+        <Route path="/moh/home" element={<MOHHome />}/>
+        <Route path="/citizen/report-site" element={<ReportSites/>}/>
       </Routes>
     </Router>
   );
