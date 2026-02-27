ec6e7c9916829a9c2a255c0a0b1dba4e48555482
induwara <udith.20241131@iit.ac.lk>
2026-02-27 22:56:46 +0530

    add-route-to-get-all-dengue-cases

--- backend/src/routes/reportCases.routes.js
+++ backend/src/routes/reportCases.routes.js
@@ -2,12 +2,15 @@ import express from 'express'
 const router = express.Router();
 
 // import controller
-import { submitReportCases } from "../controllers/reportCases.controller.js";
+import { submitReportCases, getDengueCases } from "../controllers/reportCases.controller.js";
 
 // Health-check 
 router.get("/report-case", (req, res) => {
   res.json({ status: "ok", message: "report-case endpoint is reachable" });
 });
+
+// Step 3: Define route for getting all reports
+router.get("/report-cases", getDengueCases);
 
 // Define route for creating a report
 router.post("/report-case", submitReportCases);
