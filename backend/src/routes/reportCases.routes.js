8d4f58e91672f1d6dd072babb28d27f296ce128a
induwara <udith.20241131@iit.ac.lk>
2026-02-28 00:12:04 +0530

    add-route-to-assign-phi

--- backend/src/routes/reportCases.routes.js
+++ backend/src/routes/reportCases.routes.js
@@ -2,7 +2,7 @@ import express from 'express'
 const router = express.Router();
 
 // import controller
-import { submitReportCases, getDengueCases, updateStatus } from "../controllers/reportCases.controller.js";
+import { submitReportCases, getDengueCases, updateStatus, assignPHI } from "../controllers/reportCases.controller.js";
 
 // Health-check 
 router.get("/report-case", (req, res) => {
@@ -14,6 +14,9 @@ router.get("/report-cases", getDengueCases);
 
 // Step 6: Define route for updating report status
 router.patch("/report-cases/:id/status", updateStatus);
+
+// Step 9: Define route for assigning PHI to a report
+router.patch("/report-cases/:id/assign", assignPHI);
 
 // Define route for creating a report
 router.post("/report-case", submitReportCases);
