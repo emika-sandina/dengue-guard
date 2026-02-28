adbda409ab55f51fe647274754e277684113084c
thinurahk <thinuraedu@gmail.com>
2026-02-28 15:01:34 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/pages/citizen/Home Page/CitizenHome.jsx
+++ frontend/src/pages/citizen/Home Page/CitizenHome.jsx
@@ -6,28 +6,26 @@ import symptomIcon from "../../../assets/symptomreport.svg";
 import heatmapIcon from "../../../assets/heatmap.svg";
 import educationIcon from "../../../assets/education.svg";
 import { useNavigate } from "react-router-dom";
-
-
 import announcementPlaceholder from "../../../assets/announcements.svg";
+import heroImg2 from "../../../../src/heroimg2.jpg";
 import { useState } from "react";
 
 function CitizenHome() {
-  
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const menu = [
     {
       icon: siteReportIcon,
       name: "Report Breeding Sites",
       description:
         "Identify and report breeding sites to help prevent mosquito breeding in your area.",
-        route: "/citizen/report-sites",
+      route: "/citizen/report-sites",
     },
     {
       icon: symptomIcon,
       name: "Report Dengue Cases",
       description:
         "Notify health authorities about potential dengue symptoms for immediate action.",
-        route: "/citizen/report-cases",
+      route: "/citizen/report-cases",
     },
     {
       icon: announcementPlaceholder,
@@ -40,7 +38,7 @@ function CitizenHome() {
       name: "Risk Heat Map",
       description:
         "Assess the risk level of specific areas through the risk heat map.",
-        route: "/citizen/risk-map",
+      route: "/citizen/risk-map",
     },
   ];
 
@@ -53,17 +51,29 @@ function CitizenHome() {
         <br></br>
         <br></br>
         <main className="main-content">
-          <p className="greeting">Hey Citizen</p>
-          <h1>Welcome to DengueGuard</h1>
+          <div className="header-section">
+            <div>
+              <h1>Welcome back</h1>
+              <p className="sub-header-text">
+                Identify and report vulnerable sites to prevent mosquito <br />
+                breeding in your area. Report suspected breeding sites now.
+              </p>
+            </div>
+            <img
+              className="heroimg"
+              src={heroImg2}
+              alt="Community members working together to identify and prevent mosquito breeding sites in residential areas, promoting public health awareness"
+            />
+          </div>
 
-          <div className="card-grid">
+          <div className="card-gridcm">
             {menu.map((menuOption, index) => (
-              
-              <div className="card"
-                    key={index}
-                    onClick={() => navigate(menuOption.route)}>
-                
-                <div className="menu-icon">
+              <div
+                className="cardcm"
+                key={index}
+                onClick={() => navigate(menuOption.route)}
+              >
+                <div className="menu-iconcm">
                   <img src={menuOption.icon} alt="" />
                 </div>
 
@@ -73,7 +83,7 @@ function CitizenHome() {
             ))}
           </div>
 
-          <div className="risk-box">
+          <div className="risk-boxcm">
             <h1>⚠️</h1>
             <div>
               <h3>Risk Level</h3>
@@ -84,13 +94,13 @@ function CitizenHome() {
             </div>
           </div>
 
-          <div className="stats">
-            <div className="stat-card">
+          <div className="statscm">
+            <div className="stat-cardcm">
               <h2>Community Report Count</h2>
               <p>Community Reports</p>
             </div>
 
-            <div className="stat-card">
+            <div className="stat-cardcm">
               <h2>Site Count</h2>
               <p>Sites Cleaned</p>
             </div>
