f701076ed6a1150a65755bfd87612eb1b3dddf3e
Dineli <dinelieka@gmail.com>
2026-03-01 07:30:07 +0530

    backend moh manage breeding sites

---
+++ backend/src/services/manageSites.service.js
@@ -0,0 +1,17 @@
+// back-end for MOH side to see the reported breeding sites
+import { supabase } from '../supabase.js';
+import dotenv from 'dotenv';
+
+//Load Variables
+dotenv.config();
+
+// Function to GET data from the table
+export const fetchReportSites = async () => {
+    const {error, data} = await supabase.from("breeding_sites").select("issue_type,location,urgency").order("created_at",{ascending:true});
+
+    if (error){
+        console.error("Error reading report sites: ", error.message);
+        return;
+    }
+    return data;
+}
