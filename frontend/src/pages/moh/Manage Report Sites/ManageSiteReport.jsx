ccfad9202a310ce678bfeb4dd9ae8e2b96b59d01
Dineli <dinelieka@gmail.com>
2026-03-01 16:11:38 +0530

    Fixed bug on fetching the data from Express API

--- frontend/src/pages/moh/Manage Report Sites/ManageSiteReport.jsx
+++ frontend/src/pages/moh/Manage Report Sites/ManageSiteReport.jsx
@@ -2,44 +2,53 @@
 
 import "./manageReportSites.css";
 import NavBar from "../../../components/common/Navbar/NavBar";
-import { fetchReportSites } from "../../../../../backend/src/services/manageSites.service";
 import React, { useEffect, useState } from "react";
 
-// TODO
+// displaying breeding side data
 const ManageReport = () => {
     const [sites, setSites] = useState([]);
 
     useEffect(() => {
-        fetchReportSites();
+      const loadBreedingSites = async () => {
+        try{
+          // fetch data from the Express API
+          const response = await fetch("http://localhost:5000/api/site-reports");
+
+          // catch any network errors here
+          if (!response.ok){
+            throw new Error ("Failed to fetch data from server");
+          }
+
+          // converts raw data to JSON object
+          const data = await response.json();
+
+          setSites(data.reportSite || []);  // if there is no data it will send an empty array
+        }catch (error){
+          console.error("Fetch error message: " + error.message);
+        }
+      };
+      loadBreedingSites();
     }, []);
-
     console.log(sites);
-    setSites(data); 
-}
 
-{/* <div className="moh-layout">
-      <NavBar role="MOH" />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <main className="main-content">
-        <p className="greeting">Hey Medical Officer of Health</p>
-        <h1>Welcome to DengueGuard</h1>
 
-        <div className="card-grid">
-          {menu.map((menuOption, index) => (
-            <div className="card">
-              <div className="menu-icon">
-                <img src={menuOption.icon} alt="" />
+    return (
+      <div className = "moh-layout">
+        <NavBar role="MOH" />
+        <main className = "main-content">
+          <h1>Reported Breeding Sites</h1>
+          <div>
+            {sites.map((site, index)=>(
+              <div className="card" key={index}>
+                <h3>{site.issue_type}</h3>
+                <p>Location: {site.location}</p>
+                <p>Urgency: {site.urgency}</p>
               </div>
-
-              <h3>{menuOption.name}</h3>
-              <p>{menuOption.description}</p>
-            </div>
-          ))}
-        </div>
-    </main>
-</div> */}
+            ))}
+          </div>
+        </main>
+      </div>
+    )
+}
 
 export default ManageReport;
