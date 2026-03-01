7508ab42462a9db0ed5a2b1fad8594fb90947301
Dineli <dinelieka@gmail.com>
2026-03-01 11:09:25 +0530

    Added manage breeding sites controller file

---
+++ backend/src/controllers/manageSites.controller.js
@@ -0,0 +1,24 @@
+import {fetchReportSites} from "../services/manageSites.service.js";
+
+export const displaySites = async (req, res) => {
+    try{
+        // await till supabase gets the data
+        const reportSite = await fetchReportSites();
+        if (reportSite.length === 0)
+            {
+            res.status(200).json({
+                message: "No sites reported"
+            });
+            return
+        }
+        res.status(200).json({
+            message: "Breeding site is displayed",
+            reportSite
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            error: "Couldn't display the breeding site, " + error.message
+        });
+    }
+}
