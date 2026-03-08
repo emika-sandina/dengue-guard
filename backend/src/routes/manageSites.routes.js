f02fb7e34b037631ca0d71fadc7072ec4ec4e1df
Dineli <dinelieka@gmail.com>
2026-03-08 12:41:01 +0530

    Added authentication check before routing to controller file

--- backend/src/routes/manageSites.routes.js
+++ backend/src/routes/manageSites.routes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 //Import the controller 
 import { displaySites } from '../controllers/manageSites.controller.js';
+import { authenticate } from '../middleware/auth.middleware.js';
 
 //Defining a get method to display the sites.
-router.get('/site-reports', displaySites);
+router.get('/site-reports',authenticate, displaySites);
 
 export default router;
