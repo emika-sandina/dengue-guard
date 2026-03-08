918e7d895d94067d26d2b5cbc6efaef4a7f2d1a4
Dineli <dinelieka@gmail.com>
2026-03-08 12:45:34 +0530

    renamed file

--- backend/src/routes/manageSites.routes.js
+++ backend/src/routes/manageSites.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //Import the controller 
 import { displaySites } from '../controllers/manageSites.controller.js';
-import { authenticate } from '../middleware/auth.middleware.js';
+import { authenticate } from '../middleware/authMS.middleware.js';
 
 //Defining a get method to display the sites.
 router.get('/site-reports',authenticate, displaySites);
