7ab46b5ccc98599e784c4dac095d3b51a3fbcc67
Dineli <dinelieka@gmail.com>
2026-03-08 12:34:56 +0530

    Added filter to display breedings sites coressponding to the moh area

--- backend/src/controllers/manageSites.controller.js
+++ backend/src/controllers/manageSites.controller.js
@@ -1,20 +1,40 @@
 // Created a safety net for in case there are no breeding sites reported, successfully displayed and error
 import {fetchReportSites} from "../services/manageSites.service.js";
+import {fetchMohLocation} from "../services/manageSites.service.js";
 
 export const displaySites = async (req, res) => {
     try{
-        // await till supabase gets the data
-        const reportSite = await fetchReportSites();
-        if (reportSite.length === 0)
-            {
+        const {token, user} = req;  // token and user from the middleware
+
+        // calls then fetchMohloaction function in the service file, which returns the moh officers id and moh area
+        const mohProfile = await fetchMohLocation(token);
+
+        if (!mohProfile){
+            return res.status(404).json({
+                message: "Officer profile not found"
+            })
+        }
+
+        // calls the fetchReportSites function in the service file, which return all the details of the sites
+        // TODO NOTE better performance, we should filter directly in the SQL query later
+        const reportSite = await fetchReportSites(token);
+
+        if (reportSite.length === 0){
             res.status(200).json({
                 message: "No sites reported"
             });
             return
         }
+
+        // filter to only diplay sites from the same moh area as the moh officer
+        const filteredSites = reportSite.filter(
+            (site) => site.moh_area?.replace("MOH-","").trim().toLowerCase() === mohProfile.moh_area.trim().toLowerCase()   // NOTE Short-term fix on the names of the moh area. best to change it on supabase
+        );
+
+        // Return the filtered list to the frontend
         res.status(200).json({
             message: "Breeding site is displayed",
-            reportSite
+            filteredSites
         });
     }
     catch(error){
