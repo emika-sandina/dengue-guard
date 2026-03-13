184b4faab9adea17452209b8aafcd79d2803ae56
Emika Sandina <emikasandina06@gmail.com>
2026-03-13 23:33:25 +0530

    updated the navbar contents

--- frontend/src/components/common/Navbar/NavBar.jsx
+++ frontend/src/components/common/Navbar/NavBar.jsx
@@ -17,17 +17,15 @@ function NavBar({ role = "Citizen" }) {
               <NavLink to="/citizen/home">Home</NavLink>
               <NavLink to="/citizen/report-sites">Report Site</NavLink>
               <NavLink to="/citizen/report-cases">Report Symptoms</NavLink>
-              <NavLink to="/citizen/risk-map">Risk Map</NavLink>
-              <NavLink to="/citizen/prevention">Prevention Tips</NavLink>
-              <NavLink to="/citizen/my-reports">My Reports</NavLink>
-              <NavLink to="/citizen/alerts">Alerts</NavLink>
+              <NavLink to="/citizen/risk-map">Risk Heat Map</NavLink>
+              <NavLink to="/citizen/my-reports">Alerts</NavLink>
+              <NavLink to="/citizen/alerts">Profile</NavLink>
             </>
           ) : (
             <>
               <NavLink to="/moh/dashboard">Dashboard</NavLink>
               <NavLink to="/moh/site-reports">Site Reports</NavLink>
               <NavLink to="/moh/case-reports">Dengue Case Reports</NavLink>
-              <NavLink to="/moh/alerts">Alerts</NavLink>
               <NavLink to="/moh/statistics">Risk Heat Map</NavLink>
               <NavLink to="/moh/send-announcements">Send Announcements</NavLink>
             </>
