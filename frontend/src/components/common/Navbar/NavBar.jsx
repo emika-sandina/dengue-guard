ffa02c5c15d2345299b45827876e0f584bc1b378
Emika Sandina <emikasandina06@gmail.com>
2026-03-14 09:35:56 +0530

    changed the routes in the navbar

--- frontend/src/components/common/Navbar/NavBar.jsx
+++ frontend/src/components/common/Navbar/NavBar.jsx
@@ -5,12 +5,10 @@ function NavBar({ role = "Citizen" }) {
   return (
     <>
       <header className="navbar">
-        
         <div className="navbar-left">
           <h2 className="logo">DengueGuard</h2>
         </div>
 
-        
         <nav className="navbar-links">
           {role === "Citizen" ? (
             <>
@@ -18,15 +16,15 @@ function NavBar({ role = "Citizen" }) {
               <NavLink to="/citizen/report-sites">Report Site</NavLink>
               <NavLink to="/citizen/report-cases">Report Symptoms</NavLink>
               <NavLink to="/citizen/risk-map">Risk Heat Map</NavLink>
-              <NavLink to="/citizen/my-reports">Alerts</NavLink>
-              <NavLink to="/citizen/alerts">Profile</NavLink>
+              <NavLink to="/citizen/alerts">Alerts</NavLink>
+              <NavLink to="/citizen/profile">Profile</NavLink>
             </>
           ) : (
             <>
               <NavLink to="/moh/dashboard">Dashboard</NavLink>
               <NavLink to="/moh/site-reports">Site Reports</NavLink>
               <NavLink to="/moh/case-reports">Dengue Case Reports</NavLink>
-              <NavLink to="/moh/statistics">Risk Heat Map</NavLink>
+              <NavLink to="/moh/risk-map">Risk Heat Map</NavLink>
               <NavLink to="/moh/send-announcements">Send Announcements</NavLink>
             </>
           )}
