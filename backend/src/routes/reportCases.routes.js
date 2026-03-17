9ee831a5cd02c036761793130b3dd814ccb0ec01
Senuthmi <tsenuthmi2007@gmail.com>
2026-03-17 16:02:21 +0530

    Added the routes to fetch patient coordinates

--- backend/src/routes/reportCases.routes.js
+++ backend/src/routes/reportCases.routes.js
@@ -2,7 +2,7 @@ import express from 'express'
 const router = express.Router();
 
 // import controller
-import { submitReportCases } from "../controllers/reportCases.controller.js";
+import { submitReportCases, getReportCaseLocations } from "../controllers/reportCases.controller.js";
 // Health-check 
 router.get("/report-case", (req, res) => {
   res.json({ status: "ok", message: "report-case endpoint is reachable" });
@@ -10,6 +10,8 @@ router.get("/report-case", (req, res) => {
 
 // Define route for creating a report
 router.post("/report-case", submitReportCases);
+// GET: Fetch all report locations
+router.get("/report-case/data", getReportCaseLocations);
 
 // export router to app.js
 export default router;
