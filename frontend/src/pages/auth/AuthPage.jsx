f3b2584992d5c532c9d64a8bab4d5415015cc5b0
Senuthmi <tsenuthmi2007@gmail.com>
2026-03-21 01:06:27 +0530

    Fixed Frontend vite variable issue

--- frontend/src/pages/auth/AuthPage.jsx
+++ frontend/src/pages/auth/AuthPage.jsx
@@ -5,7 +5,7 @@ import shieldLogo from '../../assets/shieldsvg.svg';
 import './auth.css';
 import Dropdown from '../citizen/Report Cases Page/Dropdown.jsx';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.VITE_API_URL ;
 
 const AuthPage = () => {
   const [isLogin, setIsLogin] = useState(true);
