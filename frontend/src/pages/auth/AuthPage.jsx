0da6e82121aec54313bc643d7d68cc08cc76e46f
Senuthmi <tsenuthmi2007@gmail.com>
2026-03-21 15:02:06 +0530

    Fixed the final errors

--- frontend/src/pages/auth/AuthPage.jsx
+++ frontend/src/pages/auth/AuthPage.jsx
@@ -7,7 +7,8 @@ import Dropdown from '../citizen/Report Cases Page/Dropdown.jsx';
 console.log("VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
 console.log("VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ||
+                    "http://localhost:5000"; ;
 
 
 const AuthPage = () => {
