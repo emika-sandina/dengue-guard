0a4e39bd6e256ac20c0a392c11dfcfe03ade683c
Senuthmi Thimansa <163660990+Senuthmi@users.noreply.github.com>
2026-03-22 08:46:47 +0530

    Update AuthPage.jsx

--- frontend/src/pages/auth/AuthPage.jsx
+++ frontend/src/pages/auth/AuthPage.jsx
@@ -5,11 +5,8 @@ import shieldLogo from '../../assets/shieldsvg.svg';
 import './auth.css';
 import Dropdown from '../citizen/Report Cases Page/Dropdown.jsx';
 console.log("VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
-
-
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ||
                     "http://localhost:5000"; ;
-
 
 
 const AuthPage = () => {
