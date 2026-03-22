8f8bf37dfb584ada4058382b989773b9e1ba414e
thinurahk <thinuraedu@gmail.com>
2026-03-22 12:58:48 +0530

    add heatmap common page

---
+++ frontend/src/components/common/HeatMap/heatmapCommonpage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function HeatmapCommonPage() {
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h1>Sri Lanka Dengue Risk Map</h1>
+        <p>
+          A real-time heatmap visualization of high-risk dengue clusters across
+          Sri Lanka. Use Dengue Guard to identify safe and high-risk zones.
+        </p>
+      </header>
+
+
+    </div>
+  );
+}
+
+export default HeatmapCommonPage;
